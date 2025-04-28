289975b46ebe8cab8f6bab302d61a86cee16117f
Mikul958 <michaelpikula958@gmail.com>
2025-04-27 21:29:52 -0400

    Figured out Room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	alias(libs.plugins.android.application)
 	alias(libs.plugins.kotlin.android)
 	alias(libs.plugins.kotlin.compose)
+	id("com.google.devtools.ksp")
 }
 
 android {
@@ -57,7 +58,9 @@ dependencies {
 	implementation(libs.androidx.lifecycle.viewmodel.compose)
 
 	// Add these dependencies for Room functionality
-	// TODO
+	implementation(libs.androidx.room.runtime)
+	implementation(libs.androidx.room.ktx)
+	ksp(libs.androidx.room.compiler)
 
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/example/csce546_project/PictureEntry.kt
+++ app/src/main/java/com/example/csce546_project/PictureEntry.kt
@@ -1,12 +1,11 @@
 package com.example.csce546_project
 
-// import androidx.room.Entity
-// import androidx.room.PrimaryKey
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-// @Entity(tableName = "pictures")
+@Entity(tableName = "pictures")
 data class PictureEntry (
-    // @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
     val picture: String  // TODO make picture
 )
